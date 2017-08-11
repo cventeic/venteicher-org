d305171bdc6858732eee1fbbc0f7ad610a905008
Chris Venteicher <chris@venteicher.org>
2017-08-11 14:50:51 -0500

    import image files require typescript declaration files

---
+++ src/images.d.ts
@@ -0,0 +1,20 @@
+//
+
+declare module "*.jpg" {
+  const name: string;
+  const path: string;
+  export default path
+}
+
+declare module "*.png" {
+  const name: string;
+  const path: string;
+  export default path
+}
+
+declare module "*.svg" {
+  const name: string;
+  const path: string;
+  export default path
+}
+
